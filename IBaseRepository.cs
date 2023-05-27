7744c27278cd314222c1efd9e0877cb1d0646f38
Amirhaf76 <44908316+amirhaf76@users.noreply.github.com>
2023-05-27 13:45:45 +0330

    Changing strategy.

---
+++ IBaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq.Expressions;
+
+namespace CerbiSharp.Test.Mock
+{
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : class
+    {
+        TEntity Find(params object[] keyValues);
+
+        TEntity Add(TEntity entity);
+
+        TEntity Remove(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+
+        void AddRange(params TEntity[] entities);
+
+        void AddRange(IEnumerable<TEntity> entities);
+
+
+        Task<TEntity> FindAsync(params object[] keyValues);
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+
+        Task AddRangeAsync(params TEntity[] entities);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        Task<int> SaveChangesAsync();
+
+        TEntity FindAndLoadProperties<TProperty>(
+            Expression<Func<TEntity, IEnumerable<TProperty>>> includeProperties,
+            params object[] keyValues) where TProperty : class;
+
+        Task<TEntity> FindAndLoadPropertiesAsync<TProperty>(
+            Expression<Func<TEntity, IEnumerable<TProperty>>> includeProperties,
+            params object[] keyValues) where TProperty : class;
+
+
+
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            params string[] includeProperties);
+
+        Task<IEnumerable<TEntity>> GetAsync(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            params string[] includeProperties);
+
+
+    }
+}
--- MockExtension.cs
+++ MockExtension.cs
@@ -1,78 +1,58 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using MockQueryable.Moq;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Moq;
 
 namespace CerbiSharp.Test.Mock
 {
-    public class MockExtension
+    public static class MockExtension
     {
-        public static Mock<DbContext> CreateMockDbContextAndSetItUp<TEntity>(List<TEntity> entities) where TEntity : class
+        public static Mock<TRepository> CreateMockBaseRepository<TEntity, TRepository>(List<TEntity> entities)
+           where TRepository : class, IBaseRepository<TEntity>
+           where TEntity : class
         {
-            var mockDbSet = entities.AsQueryable().BuildMockDbSet();
+            var mockIBaseRepository = new Mock<TRepository>();
 
-            mockDbSet = SetUpMockDbSet(entities, mockDbSet);
-
-            var dbContext = new Mock<DbContext>();
-
-            dbContext.Setup(db => db.Set<TEntity>()).Returns(mockDbSet.Object);
-
-            return dbContext;
-        }
-
-        public static Mock<DbContext> CreateMockDbContextAndSetItUp<TKey, TEntity>(Dictionary<TKey, TEntity> keyValues, Func<TEntity, TKey> getKey)
-            where TEntity : class
-            where TKey : notnull
-        {
-            var mockDbSet = keyValues.Values.AsQueryable().BuildMockDbSet();
-
-            mockDbSet = SetUpMockDbSet(keyValues, mockDbSet, getKey);
-
-            var dbContext = new Mock<DbContext>();
-
-            dbContext.Setup(db => db.Set<TEntity>()).Returns(mockDbSet.Object);
-
-            return dbContext;
-        }
+            mockIBaseRepository
+                .Setup(x => x.SaveChangesAsync())
+                .ReturnsAsync(entities.Count);
 
 
-        public static Mock<DbSet<TEntity>> SetUpMockDbSet<TEntity>(List<TEntity> entities, Mock<DbSet<TEntity>> mock)
-            where TEntity : class
-        {
-            mock
+            mockIBaseRepository
                 .Setup(x => x.Remove(It.IsAny<TEntity>()))
                 .Callback((TEntity entity) => entities.Remove(entity))
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
+                .Returns((TEntity entity) => entity);
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.Add(It.IsAny<TEntity>()))
                 .Callback((TEntity entity) => entities.Add(entity))
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
+                .Returns((TEntity entity) => entity);
 
-            mock
-                .Setup(x => x.AddAsync(It.IsAny<TEntity>(), It.IsAny<CancellationToken>()))
-                .Callback((TEntity e, CancellationToken ct) => entities.Add(e))
-                .Returns((TEntity e, CancellationToken ct) => CreateEntityEntryAsync(e));
+            mockIBaseRepository
+                .Setup(x => x.AddAsync(It.IsAny<TEntity>()))
+                .Callback((TEntity entity) => entities.Add(entity))
+                .ReturnsAsync((TEntity entity) =>
+                {
+                    return entities.FirstOrDefault(x => x == entity);
+                });
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.AddRange(It.IsAny<TEntity[]>()))
                 .Callback((TEntity[] es) => entities.AddRange(es));
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.AddRange(It.IsAny<IEnumerable<TEntity>>()))
                 .Callback((IEnumerable<TEntity> es) => entities.AddRange(es));
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.AddRangeAsync(It.IsAny<TEntity[]>()))
                 .Callback((TEntity[] es) => entities.AddRange(es))
                 .Returns(Task.CompletedTask);
 
-            mock
-                .Setup(x => x.AddRangeAsync(It.IsAny<IEnumerable<TEntity>>(), It.IsAny<CancellationToken>()))
-                .Callback((IEnumerable<TEntity> es, CancellationToken ct) => entities.AddRange(es))
+            mockIBaseRepository
+                .Setup(x => x.AddRangeAsync(It.IsAny<IEnumerable<TEntity>>()))
+                .Callback((IEnumerable<TEntity> es) => entities.AddRange(es))
                 .Returns(Task.CompletedTask);
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.Update(It.IsAny<TEntity>()))
                 .Callback((TEntity entity) =>
                 {
@@ -87,7 +67,7 @@ namespace CerbiSharp.Test.Mock
 
                     throw new Exception("There is no entity like entered entity!");
                 })
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
+                .Returns((TEntity entity) => entity);
 
             TEntity findEntity(params object[] objs)
             {
@@ -102,138 +82,45 @@ namespace CerbiSharp.Test.Mock
 
                     var property = entityType.GetProperty("Id");
 
-                    return entities.Find(e => (int)property.GetValue(e) == id);
+                    return entities.Find(e => ((int)property.GetValue(e)) == id);
                 }
 
                 throw new Exception("Obj type must be int!");
             }
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.Find(It.IsAny<object[]>()))
                 .Returns((object[] objs) => findEntity(objs));
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.FindAsync(It.IsAny<object[]>()))
                 .ReturnsAsync((object[] objs) => findEntity());
 
-            return mock;
+            return mockIBaseRepository;
         }
 
-        public static Mock<DbSet<TEntity>> SetUpMockDbSet<TKey, TEntity>(Dictionary<TKey, TEntity> dict, Mock<DbSet<TEntity>> mock, Func<TEntity, TKey> getKey)
+        public static Mock<TRepository> CreateMockBaseRepository<TKey, TEntity, TRepository>(Dictionary<TKey, TEntity> dict, Func<TEntity, TKey> getKey)
+            where TRepository : class, IBaseRepository<TEntity>
             where TEntity : class
-            where TKey : notnull
         {
-            mock
-                .Setup(x => x.Remove(It.IsAny<TEntity>()))
-                .Callback((TEntity e) => dict.Remove(getKey(e)))
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
+            var mockIBaseRepository = new Mock<TRepository>();
 
-            mock
-                .Setup(x => x.Add(It.IsAny<TEntity>()))
-                .Callback((TEntity e) => dict.Add(getKey(e), e))
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
-
-            mock
-                .Setup(x => x.AddAsync(It.IsAny<TEntity>(), It.IsAny<CancellationToken>()))
-                .Callback((TEntity e, CancellationToken ct) => dict.Add(getKey(e), e))
-                .Returns((TEntity e, CancellationToken ct) => CreateEntityEntryAsync(e));
-
-            mock
-                .Setup(x => x.AddRange(It.IsAny<TEntity[]>()))
-                .Callback((TEntity[] es) =>
-                {
-                    foreach (var e in es)
-                    {
-                        dict.Add(getKey(e), e);
-                    }
-                });
-
-            mock
-                .Setup(x => x.AddRange(It.IsAny<IEnumerable<TEntity>>()))
-                .Callback((IEnumerable<TEntity> es) =>
-                {
-                    foreach (var e in es)
-                    {
-                        dict.Add(getKey(e), e);
-                    }
-                });
-
-            mock
-                .Setup(x => x.AddRangeAsync(It.IsAny<TEntity[]>()))
-                .Callback((TEntity[] es) =>
-                {
-                    foreach (var e in es)
-                    {
-                        dict.Add(getKey(e), e);
-                    }
-                })
-                .Returns(Task.CompletedTask);
-
-            mock
-                .Setup(x => x.AddRangeAsync(It.IsAny<IEnumerable<TEntity>>(), It.IsAny<CancellationToken>()))
-                .Callback((IEnumerable<TEntity> es, CancellationToken ct) =>
-                {
-                    foreach (var e in es)
-                    {
-                        dict.Add(getKey(e), e);
-                    }
-                })
-                .Returns(Task.CompletedTask);
-
-            mock
-                .Setup(x => x.Update(It.IsAny<TEntity>()))
-                .Callback((TEntity e) => { dict[getKey(e)] = e; })
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
-
-            TEntity findEntity(params object[] objs)
-            {
-                if (objs.Length > 1)
-                {
-                    throw new Exception("For more than one key there is no behavior!");
-                }
-
-                if (objs[0] is TKey key)
-                {
-                    dict.TryGetValue(key, out TEntity entity);
-
-                    return entity;
-                }
-
-                throw new Exception($"Obj type must be {typeof(TKey).Name}!");
-            }
-
-            mock
-                .Setup(x => x.Find(It.IsAny<object[]>()))
-                .Returns((object[] objs) => findEntity(objs));
-
-            mock
-                .Setup(x => x.FindAsync(It.IsAny<object[]>()))
-                .ReturnsAsync((object[] objs) => findEntity());
-
-            return mock;
-        }
-
-
-        public static Mock<DbSet<TEntity>> CreateMockRepository<TKey, TEntity, TRepository>(Dictionary<TKey, TEntity> dict, Mock<DbSet<TEntity>> mock, Func<TEntity, TKey> getKey)
-            where TEntity : class
-            where TKey : notnull
-        {
-            mock
+            mockIBaseRepository
                .Setup(x => x.Remove(It.IsAny<TEntity>()))
                .Callback((TEntity e) => dict.Remove(getKey(e)))
-               .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
+               .Returns((TEntity e) => CreateEntityEntry(e).Entity);
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.Add(It.IsAny<TEntity>()))
                 .Callback((TEntity e) => dict.Add(getKey(e), e))
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
+                .Returns((TEntity e) => CreateEntityEntry(e).Entity);
 
-            mock
-                .Setup(x => x.AddAsync(It.IsAny<TEntity>(), It.IsAny<CancellationToken>()))
-                .Callback((TEntity e, CancellationToken ct) => dict.Add(getKey(e), e))
-                .Returns((TEntity e, CancellationToken ct) => CreateEntityEntryAsync(e));
+            mockIBaseRepository
+                .Setup(x => x.AddAsync(It.IsAny<TEntity>()))
+                .Callback((TEntity e) => dict.Add(getKey(e), e))
+                .Returns((TEntity e) => Task.FromResult(CreateEntityEntry(e).Entity));
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.AddRange(It.IsAny<TEntity[]>()))
                 .Callback((TEntity[] es) =>
                 {
@@ -243,7 +130,7 @@ namespace CerbiSharp.Test.Mock
                     }
                 });
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.AddRange(It.IsAny<IEnumerable<TEntity>>()))
                 .Callback((IEnumerable<TEntity> es) =>
                 {
@@ -253,7 +140,7 @@ namespace CerbiSharp.Test.Mock
                     }
                 });
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.AddRangeAsync(It.IsAny<TEntity[]>()))
                 .Callback((TEntity[] es) =>
                 {
@@ -264,9 +151,9 @@ namespace CerbiSharp.Test.Mock
                 })
                 .Returns(Task.CompletedTask);
 
-            mock
-                .Setup(x => x.AddRangeAsync(It.IsAny<IEnumerable<TEntity>>(), It.IsAny<CancellationToken>()))
-                .Callback((IEnumerable<TEntity> es, CancellationToken ct) =>
+            mockIBaseRepository
+                .Setup(x => x.AddRangeAsync(It.IsAny<IEnumerable<TEntity>>()))
+                .Callback((IEnumerable<TEntity> es) =>
                 {
                     foreach (var e in es)
                     {
@@ -275,10 +162,10 @@ namespace CerbiSharp.Test.Mock
                 })
                 .Returns(Task.CompletedTask);
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.Update(It.IsAny<TEntity>()))
                 .Callback((TEntity e) => { dict[getKey(e)] = e; })
-                .Returns((TEntity e) => CreateMockEntityEntry(e).Object);
+                .Returns((TEntity e) => CreateEntityEntry(e).Entity);
 
             TEntity findEntity(params object[] objs)
             {
@@ -297,32 +184,27 @@ namespace CerbiSharp.Test.Mock
                 throw new Exception($"Obj type must be {typeof(TKey).Name}!");
             }
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.Find(It.IsAny<object[]>()))
                 .Returns((object[] objs) => findEntity(objs));
 
-            mock
+            mockIBaseRepository
                 .Setup(x => x.FindAsync(It.IsAny<object[]>()))
                 .ReturnsAsync((object[] objs) => findEntity());
 
-            return mock;
+            return mockIBaseRepository;
         }
 
 
-        public static ValueTask<EntityEntry<TEntity>> CreateEntityEntryAsync<TEntity>(TEntity entity) where TEntity : class
-        {
-            return new ValueTask<EntityEntry<TEntity>>(CreateMockEntityEntry(entity).Object);
-        }
-
-
-        public static Mock<EntityEntry<TEntity>> CreateMockEntityEntry<TEntity>(TEntity entity) where TEntity : class
+        public static EntityEntry<TEntity> CreateEntityEntry<TEntity>(TEntity entity) where TEntity : class
         {
             var mockEntity = CreateMockEntityEntry<TEntity>();
 
             mockEntity.Setup(x => x.Entity).Returns(entity);
 
-            return mockEntity;
+            return mockEntity.Object;
         }
+
 
         private static Mock<EntityEntry<TEntity>> CreateMockEntityEntry<TEntity>() where TEntity : class
         {
